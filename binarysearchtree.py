b1a11d80619cc546ac177ba3ed3eb008b60cad57
Damian Phung <damianphung@gmail.com>
2019-06-10 14:17:38 +1000

    Test commit (vscode)

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -1,13 +1,14 @@
 #https://www.tutorialspoint.com/python/python_binary_search_tree.htm
 
+import tensorflow as tf
+
 class Node:
-
     def __init__(self, data):
-
         self.left = None
         self.right = None
         self.data = data
-
+        
+        
     def search(self, data):
         """
                  6
@@ -15,12 +16,17 @@ class Node:
         """
         if ( data < self.data ):
             if ( self.left is None ):
-                self.left = Node(data)
+                print("cannot find {}".format(data))
             else:
                 self.left.search(data)
+        elif ( data > self.data ):
+            if ( self.right is None ):
+                print("cannot find {}".format(data))
+            else:
+                self.right.search(data)
         else:
-            # ( data > self.data )
-            self.right.search(data)
+            # Found it
+            print ("Found {}".format(data))
 
     def insert(self, data):
         if self.data:
@@ -36,10 +42,25 @@ class Node:
                     self.right.insert(data)
         else:
             self.data = data
-     
-    
 
-        if self.data < data:
-            return self.left.search(data) 
-        else:
-            return self.right.search(data)
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data)
+        if self.right:
+            self.right.PrintTree()
+
+def main():
+    root = Node(6)
+    root.insert(7)
+    root.insert(2)
+    root.insert(100)
+    root.insert(1)
+    root.insert(8)
+    root.insert(13)
+    root.insert(15)
+    root.PrintTree()
+    root.search(2)
+
+
+main()
